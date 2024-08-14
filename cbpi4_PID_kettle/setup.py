66559a290f0c3e35d36b8578eba2b58a98fbb73a
coop <squamishcoop@gmail.com>
2024-08-14 11:28:53 -0700

    moved to pid.py lib and updated setup file

--- cbpi4_PID_kettle/setup.py
+++ cbpi4_PID_kettle/setup.py
@@ -1,15 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='cbpi4_PID_kettle',
-      version='0.0.1',
-      description='CraftBeerPi Plugin',
-      author='',
-      author_email='',
-      url='',
-      include_package_data=True,
-      package_data={
-        # If any package contains *.txt or *.rst files, include them:
-      '': ['*.txt', '*.rst', '*.yaml'],
-      'cbpi4_PID_kettle': ['*','*.txt', '*.rst', '*.yaml']},
-      packages=['cbpi4_PID_kettle'],
-     )
+setup(
+    name='cbpi4_PID_kettle',
+    version='0.0.1',
+    description='CraftBeerPi4 Plugin for PID controlled kettle',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/cbpi4_PID_kettle',
+    include_package_data=True,
+    package_data={
+        '': ['*.txt', '*.rst', '*.yaml'],
+        'cbpi4_PID_kettle': ['*', '*.txt', '*.rst', '*.yaml'],
+    },
+    packages=find_packages(),
+    install_requires=['cbpi4'],
+    entry_points={'cbpi4': ['cbpi4_PID_kettle = cbpi4_PID_kettle:setup']},
+)
